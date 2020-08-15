b21d2cb199131ceb2e24884e8dd72872d2ab7ac5
Magdiel Dantas <magdielndantas@gmail.com>
2020-08-15 01:46:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Layout from './components/Layout'
+import Layout from './components/layout'
 import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
